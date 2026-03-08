1e21060467a213d062a2d5080ed099d8e4f88588
jozeph14-dev <yuusuf6163@gmail.com>
2026-03-09 01:31:28 +0300

    exercise-10

---
+++ exercise-10/exercise10.js
@@ -0,0 +1,14 @@
+let library = [
+    {
+        title: "qisooyinka nabiyada",
+        author: " mustaphe xaaji ahmett",
+        year: 2020
+    },
+    {
+        title: "dhambalada qur'aanka",
+        author: "yusuf mohamett",
+        year: 2022
+    }
+]
+console.log(library[0].title)
+console.log(library[1].author)
