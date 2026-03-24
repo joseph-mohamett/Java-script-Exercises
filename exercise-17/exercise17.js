38517e5ff6075cbe2c49b7cbec3ce0d8570fb5ef
jozeph14-dev <yuusuf6163@gmail.com>
2026-03-24 08:50:00 +0300

    exercise-17

---
+++ exercise-17/exercise17.js
@@ -0,0 +1,13 @@
+// exercise 17
+
+let temperature = 10;
+
+if (temperature <0){
+    console.log("the temperature is very cold")
+}else if(temperature < 15){
+    console.log("the temperature is cold")
+}else if(temperature < 25){
+    console.log("the temerature is warm")
+}else{
+    console.log("the temperature is hot")
+}
