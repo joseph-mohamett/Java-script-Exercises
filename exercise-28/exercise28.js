6b9b549f3c82b5768b44199aa34b1d032933b40c
jozeph14-dev <yuusuf6163@gmail.com>
2026-03-31 00:53:15 +0300

    exercise-28

---
+++ exercise-28/exercise28.js
@@ -0,0 +1,32 @@
+// exercise-27
+
+function getUserInfo(){
+    return new Promise((success,failure)=>{
+        setTimeout(()=>{
+            const isSuccessful = true;
+            if (isSuccessful){
+                success({id: "c12002", name:"mohamett", class:"ca2213"})
+            }else{
+                failure("fialed the getUser data")
+            }
+        },2000)
+    })
+}
+
+// getUserInfo()
+// .then((info)=>{
+//     console.log("successfully",info)
+// })
+// .catch((error)=> console.log(error))
+
+async function loadUserData() {
+    try{
+        const getUser = await getUserInfo ()
+        console.log(getUser)
+    } catch(err){
+        console.log(err)
+    }
+}
+
+loadUserData()
+
