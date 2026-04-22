8dc3c03902660546a44d2874b8873c10d1f8be29
jozeph14-dev <yuusuf6163@gmail.com>
2026-04-23 00:43:21 +0300

    exercise-32

---
+++ exercise-32/exercise32.js
@@ -0,0 +1,13 @@
+// exercise-32
+
+// Query Selector By Id
+const head = document.querySelector("#header");
+console.log(head)
+
+// Query SelectoAll By ClassName
+const pragraph = document.querySelectorAll('.paragraph')
+console.log(pragraph)
+
+// Query SelectorAll by TagName
+const button = document.querySelectorAll('button')
+console.log(button)
