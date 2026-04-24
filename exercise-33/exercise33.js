23c8cb54454646cbd137cd015319b0884712ac53
jozeph14-dev <yuusuf6163@gmail.com>
2026-04-24 18:09:27 +0300

    exercise-33

---
+++ exercise-33/exercise33.js
@@ -0,0 +1,9 @@
+// exercise-33
+
+function changeContent(){
+    head.textContent = `Exercise Of Thirty-three`
+}
+
+function changeElement(){
+    paragraph.innerHTML = `Exercise Thirty-three improves strength, endurance, and focus while helping the body stay active and healthy every day.`
+}
